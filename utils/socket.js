3d935953650b8e8f8d10b4064a589b8e6aec4024
Ram Sahu <ramsahu321@gmail.com>
2024-03-16 19:44:22 +0530

    update url of frontend

--- utils/socket.js
+++ utils/socket.js
@@ -4,7 +4,7 @@ const socketIo = require('socket.io');
 const initSocket = (server) => {
     const io = socketIo(server, {
         cors: {
-            origin: "https://upachaar-prototype2-ram-sahus-projects.vercel.app",
+            origin: "https://upachaar-prototype2-g6dulxvg1-ram-sahus-projects.vercel.app",
             methods: ["GET", "POST"],
             credentials: true 
         }
